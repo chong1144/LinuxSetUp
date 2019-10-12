1cc349563edd47d7be99bda7d6bbd5f10e4c84d5
SenW <1144165853@qq.com>
2019-10-12 16:47:29 +0800

    Update some scripts

--- installTool.sh
+++ installTool.sh
@@ -1,10 +1,16 @@
-sudo apt install g++ make cmake build-essential zsh curl wget neovim -y
+sudo apt install g++ make cmake build-essential zsh curl wget neovim tmux unzip clang -y
 
+# Set up tmux
+cp ./tmux.conf ~/.tmux.conf
 
+# Set up neovim
+mkdir -p ~/.config &> /dev/null
+tar xzvf nvim.tar.bz2 
+mv nvim ~/.config/
 
-
-
+# Clean
+sudo apt autoremove
 
 # install oh-my-zsh
-# cd ~ && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# cd -
+cd ~ && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cd -
